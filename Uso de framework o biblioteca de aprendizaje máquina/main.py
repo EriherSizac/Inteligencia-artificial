73c26d3a807f9e77108b044b9512d179f06b94ee
Erick Hernandez Silva <hernandezsilvaerick@gmail.com>
2022-09-13 00:45:00 -0500

    estimacion de bias y varianza
    Se añadió la estimación de bias y varianza.
    Se cambiío de usar statsmodels a usar sklearn
    Se añadió la posibilidad de cambiar la cantidad de datos a usar para testing y train

--- Uso de framework o biblioteca de aprendizaje máquina/main.py
+++ Uso de framework o biblioteca de aprendizaje máquina/main.py
@@ -3,7 +3,8 @@ from matplotlib import pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
-import statsmodels.api as sm
+from sklearn.linear_model import LinearRegression
+from mlxtend.evaluate import bias_variance_decomp
 def open(name):
     """
     Función que abre un archivo.
@@ -13,14 +14,14 @@ def open(name):
     df = pd.read_csv(name)
     return df
 
-def data_to_split(df, y_column, x_columns=None):
+def data_to_split(df, y_column, x_columns=None, test_size = 0.10):
     if x_columns is None or x_columns == ['']:
         X = df.drop(y_column, axis=1)
     else:
         X = df[x_columns]
     y = df[y_column]
 
-    xTrain, xTest, yTrain, yTest = train_test_split(X, y, random_state= 1)
+    xTrain, xTest, yTrain, yTest = train_test_split(X, y, random_state= 1, test_size=test_size)
     return xTrain, xTest, yTrain, yTest
 
 if __name__ == '__main__':
@@ -32,30 +33,37 @@ if __name__ == '__main__':
     # Imprimimos las columnas
     print(f'Estas son las columnas del dataset: \n {df.columns}')
     # Columna de resultados Y
-    y_column = input('Nombre de la columna de resultados del data set \n¿Qué es lo que quieres '
+    y_column = input('Nombre de la columna de resulpytados del data set \n¿Qué es lo que quieres '
                      'predecir?: ')
     # Columnas de X que vamos a usar
     x_columns = input('¿Qué columnas de X quieres usar? Ingresalas separadas por comas o \n '
                       'Deja vacío para usar todas menos '
                       'la columna y: ')
     x_columns = x_columns.split(',')
+    # Le pedimos al usuario que porcentaje de entrenamiento quiere usar
+    test_size = float(input('¿Qué porcentaje de tu dataset quieres usar para probar el modelo?: '))
     # Hacemos split de los datos de entrenamiento y de prueba
-    xTrain, xTest, yTrain, yTest = data_to_split(df, y_column, x_columns)
-    # Añadimos las constantes a las X de train y test
-    xTrain = sm.add_constant(xTrain)
-    xTest = sm.add_constant(xTest)
+    xTrain, xTest, yTrain, yTest = data_to_split(df, y_column, x_columns, test_size)
     # Hacemos un fit al model para entrenarlo
-    model = sm.OLS(yTrain, xTrain).fit()
+    model = LinearRegression()
+
+    # Estimación de bias y varianza
+    mse, bias, var = bias_variance_decomp(model, xTrain.values, yTrain.values, xTest.values,
+                                          yTest.values, loss='mse', num_rounds=200,
+                                          random_seed=1)
     # Hacemos un testeo con los datos de prueba
+    #model.fit(xTrain, yTrain)
     results = model.predict(xTest)
     # Mostramos el resumen del modelo
-    print(model.summary())
-    # Mostramos el MSE de los resultados
-    print('Mean squared error: ', mean_squared_error(yTest, results))
+    print('R cuadrada: ', model.score(xTest,yTest))
+    # Mostramos los resultados de bias, varianza y mse
+    print('MSE: %.3f' % mse)
+    print('Bias: %.3f' % bias)
+    print('Variance: %.3f' % var)
     # Grafica de predichos vs esperados
     plt.figure(figsize=(10, 10))
     plt.plot(yTest.reset_index(drop=True), label='Valores esperados')
-    plt.plot(results.reset_index(drop=True), label='Valores predichos')
+    plt.plot(pd.Series(results).reset_index(drop=True), label='Valores predichos')
     plt.legend()
     plt.xlabel('Entrada')
     plt.ylabel('Resultado')
