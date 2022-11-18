b96fbadf0781ed59c5f763aae1ebb13175816af1
Mikaela Grace <mikaelagrace@users.noreply.github.com>
2022-11-17 19:09:21 -0500

    Update File.py
    no need to merge the Pull Request if you don't want to, it's just a way for me to suggest updates with inline diffs! You can merge and remove my comments, or whatever you prefer

--- NLP Module Project/Classes/File.py
+++ NLP Module Project/Classes/File.py
@@ -1,3 +1,6 @@
-class File():
+class File(): 
     def __init__(self, path):
+        # I like the idea of a helper! however, you should use the "with" construction
+        # mentioned here: https://www.programiz.com/python-programming/file-operation
+        # so that the file is automatically closed. right now, you are leaving it open! 
         self.lines = open(path).readlines()
