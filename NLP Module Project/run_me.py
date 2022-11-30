aac61d4c0e3326543ac3cdc4d88acc830b3c40d7
Erick Hernandez Silva <hernandezsilvaerick@gmail.com>
2022-11-29 19:55:06 -0600

    run_me.py comments solved

--- NLP Module Project/run_me.py
+++ NLP Module Project/run_me.py
@@ -3,20 +3,20 @@ from Classes.Ner import Ner
 from Classes.Translate import Translator
 
 print("======================= TESTING SENTIMENT ANALYSIS ===========================")
-SA = SentimentAnalysis()
+SentimentModel = SentimentAnalysis()
 print("-------------- PREDICTIONS --------------------")
-SA.predict("./datasets/tiny_movie_reviews_dataset.txt")
+SentimentModel.predict("./datasets/tiny_movie_reviews_dataset.txt")
 input("Press ENTER to continue to next model")
 
 print()
 print("======================= Ner ===========================")
 print("Do not forget to change sample size in .env file.")
-ner = Ner()
-ner.train()
-ner.plot()
+ner_model = Ner()
+ner_model.train()
+ner_model.plot()
 input("Press ENTER to continue to next model")
 
 print()
 print("=================== TRANSLATE BLEU SCORE ==================")
-t = Translator("./datasets/europarl-v7.es-en.en", "./datasets/europarl-v7.es-en.es")
-t.score_model()
+translator = Translator("./datasets/europarl-v7.es-en.en", "./datasets/europarl-v7.es-en.es")
+translator.score_model()
